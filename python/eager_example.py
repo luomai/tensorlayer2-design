0a02e11e67c45764efe094e2e6474f1c99d3cf11
lg <lg4869@outlook.com>
2018-10-16 18:03:56 +0800

    rename

--- python/eager_example.py
+++ python/eager_example.py
@@ -8,22 +8,18 @@ from base_layer import EagerPlaceholder, InputLayer, MagicAddLayer
 tf.enable_eager_execution()
 
 # Sample program
-image = tf.ones([28 * 28 * 3], tf.float32)  # :: np.array
+image1 = tf.ones([28 * 28 * 3], tf.float32)  # :: np.array
+image2 = tf.ones([28 * 28 * 3], tf.float32)  # :: np.array
 
-egr_plh = EagerPlaceholder()
+egr_plh1 = EagerPlaceholder()
 egr_plh2 = EagerPlaceholder()
 
 # begin define graph 1
-# x = InputLayer()(egr_plh)
-# y = MagicAddLayer(tf.constant(10.0))(x)
-# z = MagicAddLayer(tf.constant(15.0))(y)
-# end define graph 1
-
-# begin define graph 2
+x1 = InputLayer()(egr_plh1)
 x2 = InputLayer()(egr_plh2)
-y2 = MagicAddLayer(tf.constant(10.0))(x2)
-z2 = MagicAddLayer(tf.constant(15.0))(y2)
-# end define graph 2
+y = SumLayer()(x1, x2)
+z = MagicAddLayer(tf.constant(15.0))(y)
+# end define graph 1
 
 # this doesn't work
 # val = egr_plh(image)  # == z.forward(y.forward(x.forward(image))) internally
@@ -35,8 +31,8 @@ z2 = MagicAddLayer(tf.constant(15.0))(y2)
 
 # this might work
 val = z({
-    ph1: image1,
-    ph2: image2,
+    egr_plh1: image1,
+    egr_plh2: image2,
 })  # == z.forward(y.forward(x.forward(image))) internally
 
 print(val)
