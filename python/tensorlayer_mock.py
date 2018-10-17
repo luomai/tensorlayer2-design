121c4f168a9a0d794623f937b92e2c381528eebb
lg <lg4869@outlook.com>
2018-10-18 01:52:52 +0800

    get_variable if reuse

--- python/tensorlayer_mock.py
+++ python/tensorlayer_mock.py
@@ -3,4 +3,7 @@ import tensorflow as tf
 
 def get_variable(name, shape, train, reuse):
     # TODO: Reference the tf.keras.layers
-    return tf.Variable(tf.zeros(shape), name)
+    if reuse:
+        return tf.get_variable(name, initializer=tf.zeros(shape))
+    else:
+        return tf.Variable(tf.zeros(shape), name)
