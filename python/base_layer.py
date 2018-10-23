1970b9cab898fa4d74172971f39a1a05777ed764
Luo Mai <luo.mai.cs@gmail.com>
2018-10-23 16:30:24 +0100

    refactor base layer based on jingqing comments

--- python/base_layer.py
+++ python/base_layer.py
@@ -11,29 +11,39 @@ def get_all_weights(l):
     all_weights = []
 
     def _visit(l):
-        for instance in l.input_instances:
-            if isinstance(instance, BaseLayerInstance):
-                _visit(instance)
+        for layer in l.input_layers:
+            if isinstance(layer, BaseLayer):
+                _visit(layer)
         all_weights.extend(l.weights)
 
     _visit(l)
     return all_weights
 
 
-# Layer factory API
-class BaseLayerInstance(object):
-    def __init__(self, input_instances):
-        self._input_instances = input_instances
+# Layer API
+class BaseLayer(object):
+    def __init__(self, name):
+        # Layer constants
+        self.name = name
+
+        # Layer weight state
+        self._built = False
         self._weights = []
+
+        # Layer forward state
+        self._input_layers = None
         self._outputs = None
-
         self._inputs = []
-        for instance in input_instances:
-            for input in instance._outputs:
-                self._inputs.append(input)
 
-    def add_attribute(self, name, attr):
-        setattr(self, name, attr)
+    @abstractmethod
+    def build(self, inputs, train):
+        raise Exception(
+            "The build_weights method must be implemented by inherited class")
+
+    @abstractmethod
+    def forward(self, inputs):
+        raise Exception(
+            "The forward method must be implemented by inherited class")
 
     @property
     def all_weights(self):
@@ -51,79 +61,36 @@ class BaseLayerInstance(object):
     def inputs(self):
         return self._inputs
 
-
-class EagerLayerInstance(BaseLayerInstance):
-    def __init__(self, input_instances):
-        super().__init__(input_instances)
-
-
-class LazyLayerInstance(BaseLayerInstance):
-    def __init__(self, input_instances):
-        super().__init__(input_instances)
-
     @property
-    def input_instances(self):
-        return self._input_instances
+    def input_layers(self):
+        return self._input_layers
 
-    def __str__(self):
-        input_spec = ','.join('?' for x in self._input_instances)  # FIXME
-        output_spec = ','.join(str(y.shape) for y in self._outputs)
-        return '{%s} -> {%s}, %d weights' % (input_spec, output_spec,
-                                             len(self._weights))
-
-    def __call__(self, input_instances):
-        instance = deep_copy(self)
-        self.layer.forward(instance)
-        return instance
-
-
-# Layer API
-class BaseLayer(object):
-    def __init__(self, name):
-        self.name = name
-
-    @abstractmethod
-    def build(self, instance, train):
-        raise Exception(
-            "The build_weights method must be implemented by inherited class")
-
-    @abstractmethod
-    def forward(self, instance):
-        raise Exception(
-            "The forward method must be implemented by inherited class")
-
-    # Protected method
-    @classmethod
-    def _add_weight(self, instance, scope_name, var_name, shape, train):
+    def _add_weight(self, scope_name, var_name, shape, train):
         weight = tl.get_variable(
             scope_name=scope_name,
             var_name=var_name,
             shape=shape,
             train=train)
-        instance.weights.append(weight)  # Add into the weight collection
-        instance.add_attribute(var_name, weight)
+        self._weights.append(weight)  # Add into the weight collection
+        self.__setattr__(var_name, weight)
         return weight
 
-    def __call__(self, input_instances, train, reused_weights=None):
+    def __call__(self, input_layers, train):
         # FIXME: use "*args and **kwargs" for input parameters
-        if not isinstance(input_instances, list):
-            input_instances = [input_instances]
+        if not isinstance(input_layers, list):
+            input_layers = [input_layers]
 
-        if tf.executing_eagerly():
-            instance = EagerLayerInstance(input_instances)
-        else:
-            instance = LazyLayerInstance(input_instances)
+        self._input_layers = input_layers
+        for instance in input_layers:
+            for input in instance._outputs:
+                self._inputs.append(input)
 
-        if reused_weights is None:
-            self.build(instance, train)
-        else:
-            instance._weights = reused_weights
-            for weight in reused_weights:
-                instance.add_attribute(weight.name, weight)
+        if not self._built:
+            self.build(self._inputs, train)
 
-        instance._outputs = self.forward(instance)
+        self._outputs = self.forward(self._inputs)
 
-        return instance
+        return self
 
 
 class MagicalDenseLayer(BaseLayer):
@@ -132,17 +99,17 @@ class MagicalDenseLayer(BaseLayer):
         self._add_constant = add_constant
         self._n_class = n_class
 
-    def build(self, instance, train):
+    def build(self, inputs, train):
         shape = []
-        for dim in instance.inputs[0].shape[1:]:
+        for dim in inputs[0].shape[1:]:
             shape.append(int(dim))
         shape.append(int(self._n_class))
-        self._add_weight(instance, self.name, "w1", tuple(shape), train)
+        self._add_weight(self.name, "w1", tuple(shape), train)
 
-    def forward(self, instance):
+    def forward(self, inputs):
         outputs = []
-        for input in instance.inputs:
-            y = tf.matmul(input, instance.w1)
+        for input in inputs:
+            y = tf.matmul(input, self.w1)
             z = tf.add(y, self._add_constant)
             outputs.append(z)
         return outputs
@@ -152,16 +119,12 @@ class InputLayer(BaseLayer):
     def __init__(self, name="input"):
         super().__init__(name)
 
-    def build(self, instance):
+    def build(self, inputs, train):
         pass
 
-    def forward(self, instance):
-        return instance.inputs()
+    def forward(self, inputs):
+        return inputs
 
     def __call__(self, input_tensor):
-        if tf.executing_eagerly():
-            instance = EagerLayerInstance(input_instances=[])
-        else:
-            instance = LazyLayerInstance(input_instances=[])
-        instance._outputs = [input_tensor]
-        return instance
+        self._outputs = [input_tensor]
+        return self
