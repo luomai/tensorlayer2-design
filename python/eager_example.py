51cc6bd69f46052be5176dbdabe4420ccfc63a76
Jingqing  Zhang <jingqing.zhang15@imperial.ac.uk>
2018-11-05 16:30:01 +0000

    init eager example

--- python/eager_example.py
+++ python/eager_example.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
-from common_examples import simple_example, sequential_example
+from base_layer import BaseLayer
 
 tf.enable_eager_execution()
 
-# Sample program
-image = tf.ones([1, 28 * 28 * 3], tf.float32)  # :: np.array
 
-simple_example(image)
-sequential_example(image)
 
-ndarray = np.ones([1, 28 * 28 * 3], dtype=np.float32)
-simple_example(ndarray)
