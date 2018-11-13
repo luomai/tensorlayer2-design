0fcf99a93957327446a445384f063e9bc19f8b17
Hao <dhsig552@163.com>
2018-11-14 00:49:06 +0800

    Update hao_eager.py

--- python/hao_eager.py
+++ python/hao_eager.py
@@ -1,24 +1,40 @@
-# eager mode
-def disciminator():
-    inputs = tl.layers.Input([None, 100])
-    net = tl.layers.Dense(n_units=32, act=tf.nn.elu)(inputs)
-    net1 = tl.layers.Dense(n_units=1)(net)
-    net2 = tl.layers.Dense(n_units=5)(net)
-    D = tl.Model(inputs=inputs, outputs=[net1, net2])
-    return D
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-D = disciminator()
-output = D.forward(images, is_train=True)
-output = D.forward(images, is_train=False)
-# graph mode
-def disciminator(inputs, is_train):
-    net = tl.layers.Dense(n_units=32, act=tf.nn.elu)(inputs)
-    net1 = tl.layers.Dense(n_units=1)(net)
-    net2 = tl.layers.Dense(n_units=5)(net)
-    D = tl.Model(inputs=inputs, outputs=[net1, net2], is_train=is_train)
-    return D
-inputs = tf.placeholder("float32", [None, 100])
-D = disciminator(inputs, is_train=True)
-D2 = tl.Model(reuse=True, is_train=False, model=D)
-output = sess.run(D.outputs, feed_dict={inputs: images})
-output = sess.run(D2.outputs, feed_dict={inputs: images})
+import tensorflow as tf
+import tensorlayer as tl
+import numpy as np
+# import tensorflow.contrib.eager as tfe
+# from keras.datasets import cifar10
+tf.enable_eager_execution()
+
+def generator(input_shape):
+    inputs = tl.layers.Input(input_shape)
+    net = tl.layers.Dense(n_units=64, act=tf.nn.elu)(inputs)
+    net2 = tl.layers.Dense(n_units=64, act=tf.nn.elu)(net)
+
+    test = tl.layers.Dense(units=64, activation=tf.nn.elu, name="fc3")
+    print(test.weights)
+    print(test.input_shape, test.output_shape) # see more from keras: https://tensorflow.google.cn/api_docs/python/tf/keras/layers/Conv2D
+    
+    # print(net2.output) # AttributeError: 'DeferredTensor' object has no attribute 'output'
+    print(net2)
+    print(net2.shape, net2.name, net2.dtype)
+    print(tl.layers.Dense(n_units=64, act=tf.nn.elu).weights)
+
+    net = tl.layers.Dense(n_units=1)(net2)
+    G = tl.Model(inputs=inputs, outputs=[net, net2])
+    return G, net2
+
+latent_space_size = 100
+G, net2 = generator((None, latent_space_size))
+G.print_weights(True)
+G.print_layers()
+G.count_weights()
+print(G.weights)
+print(G.outputs) # keras: [<DeferredTensor 'None' shape=(?, ?, 1) dtype=float32>, <DeferredTensor 'None' shape=(?, ?, 64) dtype=float32>]
+# print(net2.output) # keras: AttributeError: 'DeferredTensor' object has no attribute 'output'
+inputs = np.ones((10, latent_space_size))
+outputs = G(inputs, is_train=True)
+outputs = G(inputs, is_train=False)
+
