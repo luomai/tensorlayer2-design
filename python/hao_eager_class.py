a66615af1c2621531c0f83bcc5cb491b7440c105
Hao <dhsig552@163.com>
2018-11-18 20:14:49 +0800

    Create hao_eager_class.py

---
+++ python/hao_eager_class.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import tensorlayer as tl
+import numpy as np
+# import tensorflow.contrib.eager as tfe
+# from keras.datasets import cifar10
+tf.enable_eager_execution()
+
+class MyModel(tl.Model):
+    def __init__(self):
+        self.inputs = tl.layers.Input([None, 100])
+        self.hidden_layers = [
+            tl.layers.Dense(n_units=32, act=tf.nn.relu)
+            tl.layers.Dense(n_units=16, act=tf.nn.relu)
+        ]
+        self.output_layer0 = tl.layers.Dense(n_units=5)
+        self.output_layer1 = tl.layers.Dense(n_units=1)
+    
+    def __call__(self, input, foo=0):
+         net = self.inputs(input)
+         for hl in self.hidden_layers:
+             net = hl(net)
+         if foo ==  0:
+             return self.output_layer0(net)
+         else
+             return self.output_layer1(net)
+
+M = MyModel()
+
+inputs = np.ones((10, 100))
+output0 = M(inputs, foo=0)
+output1 = M(inputs, foo=1)
