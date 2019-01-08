7ed045d83b65ed938777a05b72dea79b6928c447
Jingqing  Zhang <jingqing.zhang15@imperial.ac.uk>
2019-01-08 10:18:38 +0000

    hao_eager_class.py runnable but looks nonsense

--- python/hao_eager_class.py
+++ python/hao_eager_class.py
@@ -2,32 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-import tensorlayer as tl
+import tensorlayer_mock as tl
 import numpy as np
 from base_layer import Input, Dropout, Dense
 tf.enable_eager_execution()
 
 class MyModel(tl.Model):
     def __init__(self):
-        self.inputs = tl.layers.Input([None, 100])
+        # FIXME: currently the base tl.Model seems not really necessary?
+        super(MyModel, self).__init__()
+
+        self.input_layer = Input((None, 100))
         self.hidden_layers = [
-            tl.layers.Dense(n_units=32, act=tf.nn.relu)
-            tl.layers.Dense(n_units=16, act=tf.nn.relu)
+            Dense(n_units=32, act=tf.nn.relu),
+            Dense(n_units=16, act=tf.nn.relu)
         ]
-        self.output_layer0 = tl.layers.Dense(n_units=5)
-        self.output_layer1 = tl.layers.Dense(n_units=1)
-    
-    def __call__(self, input, foo=0):
-         net = self.inputs(input)
-         for hl in self.hidden_layers:
-             net = hl(net)
-         if foo ==  0:
-             return self.output_layer0(net)
-         else
-             return self.output_layer1(net)
+        self.output_layer0 = Dense(n_units=5)
+        self.output_layer1 = Dense(n_units=1)
+
+    def __call__(self, inputs, is_train):
+        # FIXME: this if looks stupid
+        # check if the network has been built
+        if self._inputs is None:
+            # Create network
+            net = self.input_layer
+            for hl in self.hidden_layers:
+                net = hl(net)
+            net0 = self.output_layer0(net)
+            net1 = self.output_layer1(net)
+            self._inputs = self.input_layer
+            self._outputs = [net0, net1]
+        # Forward
+        return super(MyModel, self).__call__(inputs, is_train)
+
 
 M = MyModel()
 
-inputs = np.ones((10, 100))
-output0 = M(inputs, foo=0)
-output1 = M(inputs, foo=1)
+inputs = np.ones((10, 100), dtype="float32")
+outputs_train = M(inputs, True)
+outputs_test = M(inputs, False)
+print(outputs_train)
+print(outputs_test)
--- python/hao_graph.py
+++ python/hao_graph.py
@@ -6,18 +6,19 @@ import tensorlayer_mock as tl
 from base_layer import Input, Dropout, Dense
 import numpy as np
 
-def disciminator(inputs_shape, is_train):
+def disciminator(inputs_shape):
     innet = Input(inputs_shape)
     net = Dense(n_units=32, act=tf.nn.relu)(innet)
     net = Dropout(keep=0.8, seed=1)(net)
     net1 = Dense(n_units=1, act=tf.nn.relu)(net)
     net2 = Dense(n_units=5, act=tf.nn.relu)(net)
-    D = tl.Model(inputs=innet, outputs=[net1, net2], is_train=is_train)
+    D = tl.Model(inputs=innet, outputs=[net1, net2])
     return D
 
 inputs = tf.placeholder(shape=[None, 100], dtype=tf.float32)
-D = disciminator(inputs_shape=[None, 100], is_train=True)
-outputs = D(inputs)
+D = disciminator(inputs_shape=[None, 100])
+outputs_train = D(inputs, is_train=True)
+outputs_test = D(inputs, is_train=False)
 # D2 = tl.Model(reuse=True, is_train=False, model=D)
 
 # D.print_weights(False)
@@ -31,7 +32,9 @@ sess.run(tf.global_variables_initializer())
 #D.print_weights(True)
 
 real_inputs = np.ones((5, 100))
-real_outputs = sess.run(outputs, feed_dict={inputs: real_inputs})
-print(real_outputs)
+real_outputs_train = sess.run(outputs_train, feed_dict={inputs: real_inputs})
+real_outputs_test = sess.run(outputs_test, feed_dict={inputs: real_inputs})
+print(real_outputs_train)
+print(real_outputs_test)
 
 # outputs = sess.run(D2.outputs, feed_dict={inputs: images})
